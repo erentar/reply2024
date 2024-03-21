33c4a80a52b57d6ad82316268ba66f5c41680926
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2024-03-21 16:58:24 +0100

    output.py working and tested

---
+++ output.py
@@ -0,0 +1,51 @@
+import os
+
+OUTPUT_DIR = "./output/"
+
+def output(state: list[list[int, str]], problem_name: str):
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    with open(OUTPUT_DIR + problem_name + ".txt", "w") as f:
+        for id, coordx, coordy in state:
+            f.write(id + " " + str(coordx) + " " + str(coordy) + "\n")
+
+
+if __name__ == "__main__":
+    """
+    3 3 4
+    B 4 4
+    C 4 3
+    5 4 2
+    A 5 2
+    C 5 1
+    5 5 0
+    6 6 0
+    C 6 1
+    9 6 2
+    F 7 3
+    F 7 4
+    F 7 5
+    A 7 6
+    9 5 6
+    E 5 5
+    6 5 4
+    """
+    output([
+        ["3", 3, 4],
+        ["B", 4, 4],
+        ["C", 4, 3],
+        ["5", 4, 2],
+        ["A", 5, 2],
+        ["C", 5, 1],
+        ["5", 5, 0],
+        ["6", 6, 0],
+        ["C", 6, 1],
+        ["9", 6, 2],
+        ["F", 7, 3],
+        ["F", 7, 4],
+        ["F", 7, 5],
+        ["A", 7, 6],
+        ["9", 5, 6],
+        ["E", 5, 5],
+        ["6", 5, 4]
+    ],
+           "exemple_test")
