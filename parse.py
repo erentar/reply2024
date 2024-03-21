af48222f7cf53e1444e3e9a4cd840a53a506724e
erentar <erentar2002@gmail.com>
2024-03-21 17:01:32 +0100

    parser

--- parse.py
+++ parse.py
@@ -18,3 +18,20 @@ silverPoints = listToDict(silverPoints,["x","y","score"])
 
 tiles = list1[goldenPointsCount+1+silverPointsCount:goldenPointsCount+1+silverPointsCount+tileTypes]
 tiles = listToDict(tiles,["id","cost","types"])
+
+tileToDirection = {
+    "3":{"w","e"},
+    "5":{"s","e"},
+    "6":{"s","w"},
+    "7":{"s","e","w"},
+    "9":{"n","e"},
+    "A":{"w","n"},
+    "B":{"w","n","e"},
+    "C":{"n","s"},
+    "D":{"n","s","e"},
+    "E":{"n","s","w"},
+    "F":{"n","s","w","e"}
+}
+
+for tile in tiles:
+    tile["direction"]=tileToDirection[str(tile["id"])]
