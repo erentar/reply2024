2684c7ca77e850b168375074b56fa9ab774116b9
Antoine Jacques-Jourion <52408308+axnj2@users.noreply.github.com>
2024-03-21 20:42:52 +0100

    change in state representation

--- output.py
+++ output.py
@@ -2,15 +2,16 @@ import os
 
 OUTPUT_DIR = "./output/"
 
-def output(state: list[list[int, str]], problem_name: str):
+
+def output(state: dict[tuple: str], problem_name: str):
     os.makedirs(OUTPUT_DIR, exist_ok=True)
     with open(OUTPUT_DIR + problem_name + ".txt", "w") as f:
-        for id, coordx, coordy in state:
-            f.write(id + " " + str(coordx) + " " + str(coordy) + "\n")
+        for coord in state.keys():
+            f.write(state[coord] + " " + str(coord[0]) + " " + str(coord[1]) + "\n")
 
 
 if __name__ == "__main__":
-    """
+    a = """
     3 3 4
     B 4 4
     C 4 3
@@ -29,23 +30,22 @@ if __name__ == "__main__":
     E 5 5
     6 5 4
     """
-    output([
-        ["3", 3, 4],
-        ["B", 4, 4],
-        ["C", 4, 3],
-        ["5", 4, 2],
-        ["A", 5, 2],
-        ["C", 5, 1],
-        ["5", 5, 0],
-        ["6", 6, 0],
-        ["C", 6, 1],
-        ["9", 6, 2],
-        ["F", 7, 3],
-        ["F", 7, 4],
-        ["F", 7, 5],
-        ["A", 7, 6],
-        ["9", 5, 6],
-        ["E", 5, 5],
-        ["6", 5, 4]
-    ],
-           "exemple_test")
+    b = output({
+        (3, 4): "3",
+        (4, 4): "B",
+        (4, 3): "C",
+        (5, 2): "A",
+        (5, 1): "C",
+        (5, 0): "5",
+        (6, 0): "6",
+        (6, 1): "C",
+        (6, 2): "9",
+        (7, 3): "F",
+        (7, 4): "F",
+        (7, 5): "F",
+        (7, 6): "A",
+        (5, 6): "9",
+        (5, 5): "E",
+        (5, 4): "6",
+    },
+        "exemple_test")
